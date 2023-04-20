01d175e96c1a83299eb148fb242a03ce2207338e
Chaitanya <chaitanyapradhan27@gmail.com>
2023-04-20 14:34:08 +0530

    Add Authentication Module

--- user_management/urls.py
+++ user_management/urls.py
@@ -4,5 +4,8 @@ from user_management import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup_views', views.signup_views, name='signup_views'),
+    path('login_views', views.login_views, name='login_view'),
+    path('logout_views', views.logout_views, name='logout_view'),
     path('create_user_profile', views.create_user_profile, name='create_user_profile')
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -5,10 +5,40 @@ from .forms import UserProfileForm
 from django.http import HttpResponseRedirect
 
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base.html')
+
+def signup_views(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login_view')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_views(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid credentials')
+    return render(request, 'login.html')
+
+def logout_views(request):
+    logout(request)
+    return redirect('home')
+
 def create_user_profile(request):
     if request.method == 'POST':
         form = UserProfileForm(request.POST)
