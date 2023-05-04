7269a56ccba969026ae673581cd8e33e7b9e3d3a
Chaitanya <chaitanyapradhan27@gmail.com>
2023-05-04 13:03:14 +0530

    Update Model

--- user_management/models.py
+++ user_management/models.py
@@ -2,39 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, password=None, **extra_fields):
-#         if not email:
-#             raise ValueError('The Email field must be set')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         return self.create_user(email, password, **extra_fields)
-
-# class User(AbstractBaseUser):
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     date_of_birth = models.DateField(null=True, blank=True)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']
-
-#     def __str__(self):
-#         return self.email
-
 class Profile(models.Model):
     user  = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.IntegerField(null=True, blank=True)
